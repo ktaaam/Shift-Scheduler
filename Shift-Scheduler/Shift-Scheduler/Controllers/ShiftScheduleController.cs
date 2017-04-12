7a744f9b8bda0bfdd56926c2bf9c925c8f0b9a11
alexbai94 <alexbai94@gmail.com>
2017-04-11 20:53:03 -0700

    Created view model to pass non anonymous type data to the view from controller

--- Shift-Scheduler/Shift-Scheduler/Controllers/ShiftScheduleController.cs
+++ Shift-Scheduler/Shift-Scheduler/Controllers/ShiftScheduleController.cs
@@ -1,9 +1,11 @@
 ﻿using Shift_Scheduler.Models;
+using Shift_Scheduler.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace Shift_Scheduler.Controllers
 {
@@ -14,10 +16,25 @@ namespace Shift_Scheduler.Controllers
         // GET: ShiftSchedule
         public ActionResult Index()
         {
+            DateTime startOfWeek = DateTime.Today.AddDays(-1 * (int)(DateTime.Today.DayOfWeek));
+            DateTime endOfWeek = startOfWeek.AddDays(6);
 
             ViewBag.shift = db.ShiftSchedules.ToList();
             ViewBag.employee = db.Employees.ToList();
-            
+
+            foreach (var s in db.ShiftSchedules)
+            {
+                var res = ( from ss in db.ShiftSchedules
+                          from e in db.Employees
+                          where ss.date >= startOfWeek && ss.date <= endOfWeek && ss.empShiftScheduleID == e.employeeId
+                          select new ShiftScheduleViewModel
+                          {
+                             dayOfTheWeek =  ss.dayOfTheWeek, shiftType = ss.shiftType, firstName = e.firstName, lastName = e.lastName
+                          }).ToList();
+                return View(res);
+                //ViewBag.filtered = res;
+            }
+
             return View(db.ShiftSchedules.ToList());
 
         }
